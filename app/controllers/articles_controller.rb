1ef6c36a834c776b5c84e754371e717a150f8425
Valentin Agudelo <vagudelo@altavistaed.com>
2020-05-08 18:49:00 +0000

    articles: model: Add validation for model title field.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,12 @@ class ArticlesController < ApplicationController
 	def create
   	@article = Article.new(article_params)
 
-  	@article.save
-  	redirect_to @article
-	end
+  	if @article.save
+    	redirect_to @article
+		else
+    	render 'new'
+  	end
+  end
 
 	private
   	def article_params
